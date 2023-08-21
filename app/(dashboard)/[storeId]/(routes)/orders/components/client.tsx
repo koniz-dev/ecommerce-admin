fc3e7a8f1f9ae9348abc569a4cd3e605908462f1
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2023-08-21 16:23:29 +0700

    Create Order page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { DataTable } from '@/components/ui/data-table';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+
+import { columns, OrderColumn } from './columns';
+import { Fragment } from 'react';
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <Fragment>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+
+      <Separator />
+
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </Fragment>
+  );
+};
