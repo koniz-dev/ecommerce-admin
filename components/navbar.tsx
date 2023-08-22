6e5fbb7c095d6067818db41ff1c2a3e4c0ef7936
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2023-08-22 17:15:16 +0700

    Pay action and Theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation';
 
 import StoreSwitcher from '@/components/store-switcher';
 import { MainNav } from '@/components/main-nav';
+import { ThemeToggle } from '@/components/theme-toggle';
 import prismadb from '@/lib/prismadb';
 
 const Navbar = async () => {
@@ -22,10 +23,9 @@ const Navbar = async () => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
-
         <MainNav className="mx-6" />
-
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
