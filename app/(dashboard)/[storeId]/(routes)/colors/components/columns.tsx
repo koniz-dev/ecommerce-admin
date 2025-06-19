d502bf3525a38a290caeee0ecde97e4e9fb52609
koniz-dev <20521498@gm.uit.edu.vn>
2025-06-19 09:57:39 +0700

    update position color column

--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -21,11 +21,11 @@ export const columns: ColumnDef<ColorColumn>[] = [
     header: 'Hex Value',
     cell: ({ row }) => (
       <div className="flex items-center gap-x-2">
-        {row.original.value}
         <div
           className="h-6 w-6 rounded-full border"
           style={{ backgroundColor: row.original.value }}
         />
+        <span>{row.original.value}</span>
       </div>
     ),
   },
